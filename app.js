1bb6ac727b555156ebf4313775b91ffdca812177
hemiahwu <309595700@qq.com>
2019-09-07 18:02:31 +0800

    lesson-14

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ new Vue({
   el: '#vue-app', // element
   data() {
     return {
-      error: false,
-      success: false
+      characters: ['小猪佩奇', '小羊苏西', '小兔黎贝卡', '大象艾米丽'],
+      users: [
+        { name: '小猪佩奇', age: 3 },
+        { name: '小羊苏西', age: 3 },
+        { name: '小兔黎贝卡', age: 4 },
+        { name: '大象艾米丽', age: 5 }
+      ]
     };
   },
   methods: {},
