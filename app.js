ecf60ce7e7af004891b726532848e8dbdd41dc5b
hemiahwu <309595700@qq.com>
2019-09-07 21:47:23 +0800

    lesson-17

--- app.js
+++ app.js
@@ -1,36 +1,35 @@
-// 实例化vue对象
-const one = new Vue({
-  el: '#vue-app-one', // element
+// 全局变量
+let data = {
+  name: '米斯特吴',
+  wechat: '27732357'
+};
+
+// 创建全局组件
+Vue.component('Greeting', {
+  // html模板
+  template: `
+    <p>
+    这是全局组件,可以在任何实例的容器中使用
+    我的名字是: {{name}}, 我的微信是: {{wechat}}
+    <button @click='changeName'>改名</button>
+    </p>
+  `,
+  // 属性
   data() {
-    return {
-      title: 'this is app one!!'
-    };
+    return data;
   },
-  methods: {},
-  computed: {
-    greet() {
-      return 'Hello 米修在线';
+  //  方法
+  methods: {
+    changeName() {
+      this.name = '米修在线';
     }
   }
+});
+
+const one = new Vue({
+  el: '#vue-app-one'
 });
 
 const two = new Vue({
-  el: '#vue-app-two', // element
-  data() {
-    return {
-      title: 'this is app two'
-    };
-  },
-  methods: {
-    changeAppOneTitle() {
-      one.title = '这是APP one的title';
-    }
-  },
-  computed: {
-    greet() {
-      return 'Hello 米斯特吴';
-    }
-  }
+  el: '#vue-app-two'
 });
-
-two.title = '这是app two的title内容';
