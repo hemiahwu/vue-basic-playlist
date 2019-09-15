6498f288824f06c278d4b4ecdaa26a72e983a0e2
hemiahwu <309595700@qq.com>
2019-09-15 22:15:40 +0800

    lesson-19

--- app.js
+++ app.js
@@ -11,31 +11,45 @@ new Vue({
   },
   mounted() {
     // fetch api请求接口
-    fetch('http://jsonplaceholder.typicode.com/todos')
-      .then(res => {
-        return res.json();
-      })
-      .then(todos => {
-        this.todos = todos;
-      });
+    // fetch('http://jsonplaceholder.typicode.com/todos')
+    //   .then(res => {
+    //     return res.json();
+    //   })
+    //   .then(todos => {
+    //     this.todos = todos;
+    //   });
+
+    // axios get请求接口
+    axios.get('http://jsonplaceholder.typicode.com/todos').then(res => {
+      this.todos = res.data;
+    });
   },
   methods: {
     onSubmit() {
       // console.log(this.todo);
-      fetch('http://jsonplaceholder.typicode.com/todos', {
-        method: 'POST',
-        body: JSON.stringify(this.todo),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+
+      // axios post
+      axios
+        .post('http://jsonplaceholder.typicode.com/todos', this.todo)
         .then(res => {
-          return res.json();
-        })
-        .then(todo => {
-          // console.log(todo);
-          this.todos.unshift(todo);
+          // console.log(res.data);
+          this.todos.unshift(res.data);
         });
+
+      // fetch('http://jsonplaceholder.typicode.com/todos', {
+      //   method: 'POST',
+      //   body: JSON.stringify(this.todo),
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   }
+      // })
+      //   .then(res => {
+      //     return res.json();
+      //   })
+      //   .then(todo => {
+      //     // console.log(todo);
+      //     this.todos.unshift(todo);
+      //   });
     }
   }
 });
