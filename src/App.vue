54a975b640622eee9e06437b0687fa299b5c53da
hemiahwu <309595700@qq.com>
2019-10-06 22:19:56 +0800

    lesson-30

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,32 @@
 <template>
   <div id="app">
-    <Header @titleChange="updateTitle" :title="title" />
-    <!-- 3.调用组件 -->
-    <!-- 属性传值: 值有两种情况: 1.传值 2.传引用(对象 数组) -->
-    <Users :users="users" />
-    <hr />
-    <Users :users="users" />
-    <Footer :title="title" />
+    <FormHelper>
+      <div slot="title">
+        <h2>{{title}}</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div slot="text">
+        <p>这个是文本内容</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis, ut?</p>
+      </div>
+    </FormHelper>
   </div>
 </template>
 
 <script>
-// 1.局部引用组件
-import Header from "./components/Header";
-import Users from "./components/Users";
-import Footer from "./components/Footer";
-
+import FormHelper from "./components/FormHelper";
 export default {
   name: "app",
   data() {
     return {
-      users: [
-        { name: "米斯特吴1", wechat: "27732357", show: false },
-        { name: "米斯特吴2", wechat: "27732357", show: false },
-        { name: "米斯特吴3", wechat: "27732357", show: false },
-        { name: "米斯特吴4", wechat: "27732357", show: false },
-        { name: "米斯特吴5", wechat: "27732357", show: false },
-        { name: "米斯特吴6", wechat: "27732357", show: false },
-        { name: "米斯特吴7", wechat: "27732357", show: false }
-      ],
-      title: "Vue 米斯特吴"
+      title: "这个是title"
     };
   },
   // 2.注册组件
   components: {
-    Header,
-    Users,
-    Footer
+    FormHelper
   },
-  methods: {
-    updateTitle(updatedTitle) {
-      // console.log(updatedTitle);
-      this.title = updatedTitle;
-    }
-  }
+  methods: {}
 };
 </script>
 
---
+++ src/components/FormHelper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot name="title"></slot>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, soluta?</h1>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+h2 {
+  background: lightgreen;
+}
+</style>
